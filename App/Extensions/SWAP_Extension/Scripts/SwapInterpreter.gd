e583f1977aeeb92993410d80463f68666f7ee712
Arcturus <arcturusforge@gmail.com>
2023-06-15 16:54:03 +1000

    SWAP WIP

--- App/Extensions/SWAP_Extension/Scripts/SwapInterpreter.gd
+++ App/Extensions/SWAP_Extension/Scripts/SwapInterpreter.gd
@@ -1,26 +1,62 @@
 extends Node
 
+var parserPath = "res://App/Extensions/SWAP_Extension/Scripts/Utils/Inline_Parser.gd"
+var parser
+
 #--- SYSTEM: Determines if ini file matches extension use-case.
 func data_matched(raw:String, fileName:String):
 	return false
 
 #--- SYSTEM: Called by system when the extension is enabled.
 func enable():
+	parser = ResourceLoader.load(parserPath).new()
 	pass
 
 #--- SYSTEM: Called by system when the extension is disabled.
 func disable():
+	parser = null
 	pass
 
 #--- SYSTEM: Called by system to structure interp data to be compatible with extension.
 func init_interp():
 	var interp = {
-		
+		"editType":-1, #- (-1)Comment, (0)Forms, (1)Refs, (2)Transforms
+		"notation":{
+			"name":"",
+			"comment":"",
+			"lineStart":0,
+			"lineEnd":0
+		},
+		"restrictions": [],
+		"target":"",
+		"replacements":[],
+		"transform":{
+			"position":"",
+			"rotation":"",
+			"scale":""
+		},
+		"chance":""
 	}
 	return interp
 
 #--- SYSTEM: Interpretes the raw string data to data structures used in editor.
 func raw_to_interp(raw:String):
+	var symbols = {
+		";":-1,
+		"[Forms":0,
+		"[References":1,
+		"[Transforms":2,
+		"|":3
+	}
+	var parsed_lines = parser.split_and_define_lines(raw, symbols)
+	for line in parsed_lines:
+		match line.type:
+			-1:#- Comment
+			0:#- Form Header
+			1:#- Reference Header
+			2:#- Transform Header
+			3:#- Edit
+			
 	return {}
 
 #--- SYSTEM: Compiles the interp data into raw string format for ini-extension syntax.
---
+++ App/Extensions/SWAP_Extension/Scripts/UI/Panels/FormsPanel.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+#-- Scene Refs
+onready var v_box_container = $VBoxContainer/VBoxContainer
+
+#-- Prefabs
+var restriction_node = "res://App/Extensions/SWAP_Extension/Interfaces/Nodes/RestrictionNode.tscn"
+
+func set_data(edit):
+	pass
+
+func get_data():
+	var results = []
+	return results
